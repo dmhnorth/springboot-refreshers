2481169e8e3123f95c45c7cd757fa0e99423faab
Dave <dmhnorth@Daves-MacBook-Pro.local>
2025-10-27 17:06:02 +0000

    Adds Java Spring boot basic example

---
+++ JavaDemo/src/main/java/com/example/JavaDemo/JavaDemoApplication.java
@@ -0,0 +1,33 @@
+package com.example.JavaDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController //This is the bit you're likely to forget
+public class JavaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDemoApplication.class, args);
+	}
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
+
+/*
+To manually run this:
+./gradlew bootRun
+
+Then navigate to
+
+http://localhost:8080/hello
+
+An Optional parameter
+http://localhost:8080/hello?name=Dave
+ */
